5fceb0c8aaa408af16c1a8c722f673f56e0666b5
funilrys <contact@funilrys.com>
2024-09-08 18:39:40 +0200

    Update dependencies.

--- ultimate_hosts_blacklist/whitelist/__init__.py
+++ ultimate_hosts_blacklist/whitelist/__init__.py
@@ -40,7 +40,7 @@ from colorama import init as initiate
 
 from ultimate_hosts_blacklist.whitelist.core import Core
 
-VERSION = "3.27.1"
+VERSION = "3.27.3"
 
 environ["PYFUNCEBLE_CONFIG_DIR"] = gettempdir()
 environ["PYFUNCEBLE_AUTO_CONFIGURATION"] = "YES"
